1e736f4a3a4bc13cdb65724493a00884bfc34987
luisresende13 <luisresende13@gmail.com>
2022-01-31 03:16:02 -0300

    PORT error debugged

--- index.js
+++ index.js
@@ -139,8 +139,8 @@ app.get('/', (req, res) => {
 })
 
 
-// const PORT = process.env.PORT
-const PORT = 3000;
+const PORT = process.env.PORT
+// const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log('Server started at port ' + PORT + '.')
